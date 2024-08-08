a4a92f50bc40bfb02d33ff05a6d9c61d53357362
Prasanth K S <ksprasanthksp10@gmail.com>
2024-08-08 21:27:45 +0530

    Backend Code

---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var mdb = require('mongoose')
+var User = require('./models/userSchema')
+var app = express();
+mdb.connect("mongodb://localhost:27017/SKCET")
+var db = mdb.connection
+db.once("open",()=>{
+    console.log("MongoDB Connection Successful");
+})
+app.get("/", (request, response) => {
+  response.send("Welcome to Backend Server");
+});
+
+app.get("/signup",(request, response)=>{
+    var [email, username, name, password] = ["rampex@skcet.ac.in","rampex","RAMPeX","123456"];
+    var newUser = new User({
+        email:email,
+        username:username,
+        name:name,
+        password:password
+    }) 
+    newUser.save().then(()=>{
+        console.log(response);
+        return response.json({message:"User Added"})
+    }).catch((e)=>{
+        console.log(e);
+        return response.json({message:e})
+    })
+})
+app.listen(3001, () => console.log("Backend Started"));
